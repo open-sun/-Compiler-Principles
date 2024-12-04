d3cbcc1d111c10d931f931af69e0ead6fcf4ea04
孙启森2212422 <1695730274@qq.com>
2024-12-04 13:03:27 +0000

    Update file basicmen2reg.cpp

--- lab4/src/basicmen2reg.cpp
+++ lab4/src/basicmen2reg.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void Mem2reg::execute() {
-    for (auto func = unit->begin(); func != unit->end(); func++){
+    for (auto func = unit->begin(); func != unit->end(); func++){//形成allocalist，alloca只在entry里
         BasicBlock *temp=(*func)->getEntry();
         Instruction *ins=temp->begin();
         for(;ins!=temp->end();)
@@ -21,6 +21,7 @@ void Mem2reg::execute() {
     }
 
     for(size_t i=0;i<allocalist.size();i++)
+    //清除noload，一些普适的判断或操作直接在instruction加了函数，不过当函数里用到block的相关函数就不大行，会发生error，所以这里remove在mem这个类定义了
     {
         AllocaInstruction *alloca=dynamic_cast<AllocaInstruction*>(allocalist[i]);
         if(!alloca->isloaded())
